f6ff9e334a8bb8126d955ce8bc4ea4bd142c40a8
Jos Vermoesen <info@rv.be>
2019-10-12 06:17:03 +0200

    initial

---
+++ csNumberChecker/FormCheckers.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace csNumberChecker
+{
+    partial class FormCheckers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSepa = new System.Windows.Forms.CheckBox();
+            this.cbWithSpaces = new System.Windows.Forms.CheckBox();
+            this.lResult = new System.Windows.Forms.Label();
+            this.tbBEBankAccount = new System.Windows.Forms.TextBox();
+            this.bCheckBankBE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbSepa
+            // 
+            this.cbSepa.AutoSize = true;
+            this.cbSepa.Checked = true;
+            this.cbSepa.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbSepa.Location = new System.Drawing.Point(144, 43);
+            this.cbSepa.Name = "cbSepa";
+            this.cbSepa.Size = new System.Drawing.Size(51, 17);
+            this.cbSepa.TabIndex = 9;
+            this.cbSepa.Text = "Sepa";
+            this.cbSepa.UseVisualStyleBackColor = true;
+            // 
+            // cbWithSpaces
+            // 
+            this.cbWithSpaces.AutoSize = true;
+            this.cbWithSpaces.Location = new System.Drawing.Point(12, 43);
+            this.cbWithSpaces.Name = "cbWithSpaces";
+            this.cbWithSpaces.Size = new System.Drawing.Size(117, 17);
+            this.cbWithSpaces.TabIndex = 8;
+            this.cbWithSpaces.Text = "Format with spaces";
+            this.cbWithSpaces.UseVisualStyleBackColor = true;
+            // 
+            // lResult
+            // 
+            this.lResult.AutoSize = true;
+            this.lResult.Location = new System.Drawing.Point(9, 84);
+            this.lResult.Name = "lResult";
+            this.lResult.Size = new System.Drawing.Size(19, 13);
+            this.lResult.TabIndex = 7;
+            this.lResult.Text = "dd";
+            // 
+            // tbBEBankAccount
+            // 
+            this.tbBEBankAccount.Location = new System.Drawing.Point(12, 12);
+            this.tbBEBankAccount.Name = "tbBEBankAccount";
+            this.tbBEBankAccount.Size = new System.Drawing.Size(249, 20);
+            this.tbBEBankAccount.TabIndex = 6;
+            // 
+            // bCheckBankBE
+            // 
+            this.bCheckBankBE.Location = new System.Drawing.Point(267, 9);
+            this.bCheckBankBE.Name = "bCheckBankBE";
+            this.bCheckBankBE.Size = new System.Drawing.Size(162, 23);
+            this.bCheckBankBE.TabIndex = 5;
+            this.bCheckBankBE.Text = "BE Bank account validation";
+            this.bCheckBankBE.UseVisualStyleBackColor = true;
+            this.bCheckBankBE.Click += new System.EventHandler(this.bCheckBankBE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 111);
+            this.Controls.Add(this.cbSepa);
+            this.Controls.Add(this.cbWithSpaces);
+            this.Controls.Add(this.lResult);
+            this.Controls.Add(this.tbBEBankAccount);
+            this.Controls.Add(this.bCheckBankBE);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.CheckBox cbSepa;
+        internal System.Windows.Forms.CheckBox cbWithSpaces;
+        internal System.Windows.Forms.Label lResult;
+        internal System.Windows.Forms.TextBox tbBEBankAccount;
+        internal System.Windows.Forms.Button bCheckBankBE;
+    }
+}
+
---
+++ csNumberChecker/FormCheckers.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csNumberChecker
+{
+    public partial class FormCheckers : Form
+    {
+        public FormCheckers()
+        {
+            InitializeComponent();
+        }
+
+        private void bCheckBankBE_Click(object sender, EventArgs e)
+        {
+            lResult.Text = BankOk(tbBEBankAccount.Text, cbSepa.Checked, cbWithSpaces.Checked);
+        }
+
+        private string BankOk(string anyRekString, bool sepaFlag, bool returnFormatted)
+        {
+            int rekLength = anyRekString.Length;
+            long dPip = 0;
+            int dPip2 = 0;
+            long calcPip = 0;
+
+            bool inputIsSepa = false;
+            string rekOld = "";
+            string rekSepa = "";
+
+            switch (rekLength)
+            {
+                case 12:
+                    inputIsSepa = false;
+                    rekOld = anyRekString;
+                    break;
+
+                case 14:
+                    inputIsSepa = false;
+                    rekOld =
+                        anyRekString.Substring(0, 3) +
+                        anyRekString.Substring(4, 7) +
+                        anyRekString.Substring(12, 2);
+                    break;
+
+                case 16:
+                    inputIsSepa = true;
+                    rekSepa = anyRekString;
+                    rekOld = rekSepa.Substring(4, 12);
+                    break;
+
+                case 19:
+                    inputIsSepa = true;
+                    rekSepa =
+                        anyRekString.Substring(0, 4) +
+                        anyRekString.Substring(5, 4) +
+                        anyRekString.Substring(10, 4) +
+                        anyRekString.Substring(15, 4);
+                    rekOld = rekSepa.Substring(4, 12);
+                    break;
+
+                default:
+                    return "invalid";
+            }
+
+            // first check if rekOld is valid
+            dPip = Int64.Parse(rekOld.Substring(0, 10));
+            dPip2 = int.Parse(rekOld.Substring(10, 2));
+            calcPip = dPip - ((dPip / 97) * 97);
+
+            if (rekOld.Substring(10, 2) == "00")
+            {
+                return "invalid";
+            }
+            else if (calcPip == 0 && (rekOld.Substring(10, 2) == "97"))
+            {
+                // ok
+            }
+            else if (calcPip == dPip2)
+            {
+                // ok
+            }
+            else
+            {
+                return "invalid";
+            }
+
+            // then check if sepa version is needed
+            if (!sepaFlag)
+            {
+                if (!returnFormatted )
+                {
+                    return rekOld;
+                }
+                else
+                {
+                    rekOld =
+                        rekOld.Substring(0, 3) + " " +
+                        rekOld.Substring(3, 7) + " " +
+                        rekOld.Substring(10, 2);
+                    return rekOld;
+                }
+            }
+            else
+            {
+                if (!inputIsSepa)
+                {
+                    rekSepa = "BE00" + rekOld;
+                }
+                else if (!(rekSepa.Substring(0, 2) == "BE"))
+                {
+                    return "invalid";
+                }
+
+                // dPip = Val(Mid(rekOldString, 11, 2) + Mid(rekOldString, 11, 2) + "111400")
+                dPip = Int64.Parse(rekOld.Substring(10, 2) + rekOld.Substring(10, 2) + "111400");
+                calcPip = dPip - ((dPip / 97) * 97);
+                
+                if (!inputIsSepa)
+                {
+                    rekSepa = rekSepa.Remove(2, 2).Insert (2, (98 - calcPip).ToString("00"));
+                    // s = s.Remove(3, 2).Insert(3, "ZX");
+                }
+
+                dPip2 = int.Parse(rekSepa.Substring(2, 2));
+                if (!((98 - calcPip) == dPip2))
+                {
+                    return "invalid";
+                }
+
+                if (!returnFormatted ){
+                    return rekSepa;
+                }
+                else
+                {
+                    rekSepa =
+                        rekSepa.Substring(0, 4) + " " +
+                        rekSepa.Substring(4, 4) + " " +
+                        rekSepa.Substring(8, 4) + " " +
+                        rekSepa.Substring(12, 4);
+                    return rekSepa;
+                }
+            }
+        }
+    }
+}
